feebc7a3c5c0706e28d3aeded3b5883042fff640
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2019-05-09 21:37:54 +0300

    Add empty project with .gitignore

---
+++ LinqHomework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
