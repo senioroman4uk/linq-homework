50189bd1e5f2f599c8ffd21bd8d2deeb36983ba5
Vladyslav Romanchuk <Vladyslav.Romanchuk@wolterskluwer.com>
2019-05-10 19:18:33 +0300

    solved first task

--- LinqHomework/HomeWork.cs
+++ LinqHomework/HomeWork.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqHomework
 {
@@ -9,7 +11,11 @@ namespace LinqHomework
         // Iron Man
         public static void PrintNamesOfMoviesWithRatingLessThen8(IReadOnlyCollection<Movie> movies)
         {
-            
+            var result = movies.Where(movie => movie.Rating < 8).Select(movie => movie.Name);
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+            }
         }
         
         // Example of output:
@@ -17,7 +23,15 @@ namespace LinqHomework
         // { Genre = Adventure, Count = 3 }
         public static void PrintAmountOfMoviesByGenres(IReadOnlyCollection<Movie> movies)
         {
-            
+            var genres = movies.SelectMany(m => m.Genres, (movie, genre) => new
+            {
+                movie.Name,
+                genre
+            }).GroupBy(g => g.genre, g => g.Name);
+            foreach( var genre in genres)
+            {
+                Console.WriteLine("{{ Genre = {0}, Count = {1} }}", genre.Key, genre.Count());
+            }
         }
         
         // Example of output
@@ -26,19 +40,39 @@ namespace LinqHomework
         // etc.
         public static void PrintActorsAndTheirMovies(IReadOnlyCollection<Movie> movies)
         {
-            
+            var actors = movies.SelectMany(m => m.Actors, (movie, actor) => new
+            {
+                movie.Name,
+                actor
+            }).GroupBy(a => new
+            {
+                a.actor.FirstName,
+                a.actor.LastName
+            }, a => a.Name);
+            foreach(var actor in actors)
+            {
+                Console.WriteLine("{0} {1} {2}", 
+                    actor.Key.FirstName, 
+                    actor.Key.LastName, 
+                    actor.Count());
+            }
         }
         
         // The Adjustment Bureau 7.1
         // Iron Man 
         public static void PrintMoviesInOrderOfRating(IReadOnlyCollection<Movie> movies)
         {
-            
+            var result = movies.OrderBy(m => m.Rating);
+            foreach(var item in result)
+            {
+                Console.WriteLine("{0} {1}", item.Name, item.Rating);
+            }
         }
 
         public static void PrintNameOfMovieWithTheHighestRating(IReadOnlyCollection<Movie> movies)
         {
-            
+            var result = movies.OrderByDescending(m => m.Rating).First().Name;
+            Console.WriteLine(result);
         }
         
         // Mat Daemon 7.5
@@ -46,27 +80,65 @@ namespace LinqHomework
         // average means average rating of all movies for the actor
         public static void PrintActorsAverageRating(IReadOnlyCollection<Movie> movies)
         {
-            
+            var result = movies.SelectMany(m => m.Actors, (movie, actor) => new
+            {
+                actor,
+                movie.Rating
+            }).GroupBy(a => new
+            {
+                a.actor.LastName,
+                a.actor.FirstName
+            });
+            foreach(var item in result)
+            {
+                Console.WriteLine("{0} {1} {2}", 
+                    item.Key.FirstName, 
+                    item.Key.LastName, 
+                    item.Average(i => i.Rating));
+            }
         }
         
         // Iron Man, Avengers, ....
         public static void PrintAllMovieNamesInSingleLineSeparatedByComa(IReadOnlyCollection<Movie> movies)
         {
-            
+            var result = movies.Select(m => m.Name)
+                            .Aggregate((m1, m2) => m1 + ", " + m2);
+            Console.WriteLine(result);
         }
 
-        
+
         // Example of output "Yes"/ "No"
         public static void PrintAreThereAnyComedyMovies(IReadOnlyCollection<Movie> movies)
         {
-            
+            bool result = movies.Any(m => m.Genres.Any(g => g == "Comedy"));
+            Console.WriteLine(result ? "Yes" : "No");
         }
 
         // Robert Dawney Jr 2
         // Jake  Gyllenhaal 2
         public static void PrintActorsWithTheLargestCountOfMovies(IReadOnlyCollection<Movie> movies)
         {
-            
+            var actors = movies.SelectMany(m => m.Actors, (movie, actor) => new
+            {
+                movie.Name,
+                actor
+            }).GroupBy(a => new
+            {
+                a.actor.FirstName,
+                a.actor.LastName
+            }, a => a.Name);
+
+            var maxCount = actors.Max(a => a.Count());
+            var result = actors.Where(
+                a => a.Count() == maxCount);
+
+            foreach( var item in result)
+            {
+                Console.WriteLine("{0} {1} {2}", 
+                    item.Key.FirstName, 
+                    item.Key.LastName, 
+                    item.Count());
+            }
         }
     }
 }
--- LinqHomework/Program.cs
+++ LinqHomework/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LinqHomework
+﻿using System;
+
+namespace LinqHomework
 {
     public class Program
     {
@@ -6,31 +8,40 @@
         {
             // First task
             HomeWork.PrintNamesOfMoviesWithRatingLessThen8(MovieDatabase.Movies);
-            
+
+            Console.WriteLine(new String('-', 50));
             // Second task
             HomeWork.PrintAmountOfMoviesByGenres(MovieDatabase.Movies);
-            
+
+            Console.WriteLine(new String('-', 50));
             // Third task
             HomeWork.PrintActorsAndTheirMovies(MovieDatabase.Movies);
-            
+
+            Console.WriteLine(new String('-', 50));
             // Fourth task
             HomeWork.PrintMoviesInOrderOfRating(MovieDatabase.Movies);
-            
+
+            Console.WriteLine(new String('-', 50));
             // Fifth task
             HomeWork.PrintMoviesInOrderOfRating(MovieDatabase.Movies);
-            
+
+            Console.WriteLine(new String('-', 50));
             // 6th task
             HomeWork.PrintNameOfMovieWithTheHighestRating(MovieDatabase.Movies);
-            
+
+            Console.WriteLine(new String('-', 50));
             // 7th task
             HomeWork.PrintActorsAverageRating(MovieDatabase.Movies);
-            
+
+            Console.WriteLine(new String('-', 50));
             // 8th task
             HomeWork.PrintAllMovieNamesInSingleLineSeparatedByComa(MovieDatabase.Movies);
-            
+
+            Console.WriteLine(new String('-', 50));
             // 9th task
             HomeWork.PrintAreThereAnyComedyMovies(MovieDatabase.Movies);
-            
+
+            Console.WriteLine(new String('-', 50));
             // 10th task
             HomeWork.PrintActorsWithTheLargestCountOfMovies(MovieDatabase.Movies);
         }
