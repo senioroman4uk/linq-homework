09dfb2bc0d977702b55c7ca53454a0bc861864f2
Vladyslav Romanchuk <Vladyslav.Romanchuk@wolterskluwer.com>
2019-05-10 10:28:36 +0300

    Add Linq2GitHub project

---
+++ Linq2GitHub/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Linq2GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implement IQueryable provider that uses github rest api for querying repositories
+            // See https://developer.github.com/v3/repos/#list-user-repositories
+            // Should support sorting by `created, updated, pushed, full_name`
+            // Should support sort direction
+            // Should support filtering by type
+            // In other cases should throw not implemented
+        }
+    }
+}
